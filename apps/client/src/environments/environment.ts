1de0d0b6201db9a810a35fd3bcff33920f3ad318
Bemi Irone <bemidev@gmail.com>
2026-06-17 14:34:22 +0100

    Update Auth0 configuration in environment file

--- apps/client/src/environments/environment.ts
+++ apps/client/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   auth0: {
-    domain: 'YOUR_AUTH0_DOMAIN',
-    clientId: 'YOUR_AUTH0_CLIENT_ID',
-    audience: 'YOUR_AUTH0_AUDIENCE',
+    domain: 'dev-1zccqcnn78wikh0o.us.auth0.com',
+    clientId: 'JPgJ1SIDwEYCTsonVihhITfpVbwDnkPV',
+    audience: 'code-challenger.com',
   },
 };
